c6181a9c16c28e0082d82b1f7dea6e0fe4fa2997
brewbar2016 <brewbar2016@gmail.com>
2025-06-07 20:35:17 +0300

    feat: configurable port

--- backend/server.js
+++ backend/server.js
@@ -78,6 +78,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-server.listen(5000, () => {
-  console.log("Сервер запущен на http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
